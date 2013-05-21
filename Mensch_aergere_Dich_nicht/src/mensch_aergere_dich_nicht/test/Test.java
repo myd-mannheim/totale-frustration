24c1de5003903a8b8008de2c722fa166a76f2b7f
Maximilian Naehrlich <m.naehrlich@outlook.com>
2013-05-21 16:20:42 +0200

    mensch_aergere_dich_nicht.test und Klasse Test hinzugefügt

---
+++ Mensch_aergere_Dich_nicht/src/mensch_aergere_dich_nicht/test/Test.java
@@ -0,0 +1,7 @@
+package mensch_aergere_dich_nicht.test;
+
+//Die Klasse Test war für das erste hochladen bei Git Hub notwendig.
+//Sobald andere Klassen commited sind, kann diese Klasse gelöscht werden
+public class Test {
+
+}
