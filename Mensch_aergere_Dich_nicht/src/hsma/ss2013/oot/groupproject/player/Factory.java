b342447828e576492c61c264f0fa374248d82a66
Maximilian Naehrlich <m.naehrlich@outlook.com>
2013-06-04 09:26:45 +0200

    Kommentar zu "Factory"

--- Mensch_aergere_Dich_nicht/src/hsma/ss2013/oot/groupproject/player/Factory.java
+++ Mensch_aergere_Dich_nicht/src/hsma/ss2013/oot/groupproject/player/Factory.java
@@ -1,6 +1,10 @@
 package hsma.ss2013.oot.groupproject.player;
 
 public class Factory {
+	//TODO
+	//"Factory" scheint mir kein geschickter name zu sein: Bitte lieber z.B. GameFactory falls sie Spiele zurück gibt
+	//solange der Konstruktor nicht private ist, ist die Klasse kein Singleton (warum muss sie überhaupt singelton sein?)
+	//vlt. lieber public static Game getGame(...) 
 
     //Singleton
     static Factory factory = new Factory();
