16a0ed54eeb310a27938bd792fcafc170881cb93
Maximilian Naehrlich <m.naehrlich@outlook.com>
2013-06-07 08:46:51 +0200

    Pseudocode für AI Zugermittlung

--- Mensch_aergere_Dich_nicht/src/hsma/ss2013/oot/groupproject/player/AIPlayer.java
+++ Mensch_aergere_Dich_nicht/src/hsma/ss2013/oot/groupproject/player/AIPlayer.java
@@ -8,6 +8,37 @@ public class AIPlayer extends Player {
      * @return the piece
      */
     public Piece getPiece() {
+    	
+    	/*
+    	
+    	//Pseudo Code für AI Spielzug ermittlung
+    	def moveRatings[4]
+    			
+    	def moveRating1[x]
+    	def moveRating2[x]
+    	def moveRating3[x]
+    	def moveRating4[x]
+    	
+    	moveRatings[1] = moveRating1;
+    	moveRatings[2] = moveRating2;
+    	moveRatings[3] = moveRating3;
+    	moveRatings[4] = moveRating4;
+    	
+    	//Fülle für jeder Spielfigur das Zugbewertungs Array    	
+    	for each Spielfigur into figur
+    		def moves[] = game->getPossibleMoves(figur);
+    		for each moves into move
+    			doMoveRating(move, moveRatings[figur.number])
+    		rof
+    	rof
+    	
+    	//jetzt muss der höchst priorisierteste Zug gefunden werden der auf true sitz
+    	//überalle Spiefiguren
+    	 * 
+    	 * 
+    	 */
+    	
+    	
 	//TODO Implementierung
 	return piece;
     }
